ed7deb269fe96fdb55196a44c28b0523238ec992
keepitvideo <info@keepitvideo.de>
2026-06-13 15:10:01 +0200

    ci: configure automatic deployment to GitHub Pages via GitHub Actions

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/RuslanZinevych/',
   server: {
     host: true, // Listen on all addresses, including LAN and public addresses
     cors: true,
